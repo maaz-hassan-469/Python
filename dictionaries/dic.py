550fa384d99de66d3a44f46310feed87049a3984
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-07-17 08:09:00 -0700

    Create dic.py

---
+++ dictionaries/dic.py
@@ -0,0 +1,10 @@
+student={"name":"maaz",
+         "age":20,
+         "subjects":["python","C++","C"],
+         "city":"khushab",
+         1:"hi",
+         "tuple":(1,2,3)}
+print(student)
+
+print(student["name"])
+print(student[1])
