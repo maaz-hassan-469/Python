ebbd49cc4a703c955f3790c3ddb2c1672d338ed8
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-07-18 04:02:29 -0700

    variables and typecasting

---
+++ chap 2(variables and datatypes)/typecasting.py
@@ -0,0 +1,10 @@
+# it will print its integer type 
+a=2
+b=type(a)
+print(b)
+# we are using type casting 
+# c but the type should be float
+c=3
+d=float(c)#converting the datatype of c into float
+e=type(d) #storing the type name in the e variable
+print (e)
---
+++ chap 2(variables and datatypes)/variable.py
@@ -0,0 +1,12 @@
+a=1
+b=2
+c=a+b
+print(c)
+d="maaz"
+#this is not possible because a is int and d is string
+e=a+d
+print(e)
+# it will  contcatenate two strings
+f=" hassan"
+g=d+f
+print(g)
