0bbff2c6b9a8d9382332bffff69de093addf8e2c
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-07-19 11:53:15 -0700

    list and tuples

---
+++ chap 4(list and tupple)/slicing_list.py
@@ -0,0 +1,6 @@
+fruits=["apple","banana","orange","mango"]
+print(fruits[0:2])#ending index is not inculde
+print(fruits[:2])#it means is starts from the 0 index
+print(fruits[0:])#it means is ending index is include
+print(fruits[0:3:2])#it works same as the slicing of strings
+#for the negative slicing to make it easy first convert the negative into positive but in reverse order
---
+++ chap 4(list and tupple)/tuples.py
@@ -0,0 +1,7 @@
+tuples=(1,2,3,"harry")#tuple is immutable
+#tuple[3]=2 this is not allowed in python because we cannot make any changes in the existing tuple
+print(type(tuples))
+empty_tuple=()#empty touple can be make in this way
+print(type(empty_tuple))
+single_tuple=(1,)#single value tuple can be make in this way
+print(single_tuple)
