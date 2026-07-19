bdb12a350b4232f2789d040ce7e4d6e98178870d
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-07-19 11:46:02 -0700

    string slicing

---
+++ chap 3(strings)/skipvalue.py
@@ -0,0 +1,2 @@
+word="amazing"
+print(word[1:6:2])#first solve the [1:6] then skip 2 values continuously in the variable
---
+++ chap 3(strings)/slicing.py
@@ -0,0 +1,14 @@
+name="maaz"
+nickname=name[0:2]#ignore the last index in this
+print(nickname)
+print(name[0])#print the first alphabet of the variable
+
+#negative slicing
+print(name[-3:-1])
+print(name[1:3])#we can find output of negative slicing by using this alternative method
+
+print(name[:4])#it means it start with 0 index
+print(name[0:])#and it means it end with the last index
+
+
+
