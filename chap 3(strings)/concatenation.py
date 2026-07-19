5f5567725adc9ab0a68a81be37a223e91218401a
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-07-19 11:39:41 -0700

    create concatenation.py and methods_strings.py

---
+++ chap 3(strings)/concatenation.py
@@ -0,0 +1,6 @@
+#string is immutable that we cannot make any changes to the original variable we have 
+# to create new string
+name=input("enter your name: ")
+city=input("enter your city name ")
+full_sentence="hello "+ name + " welcome to " +city
+print(full_sentence)
---
+++ chap 3(strings)/methods_strings.py
@@ -0,0 +1,7 @@
+myname="maaz is a good boy"
+print(myname.endswith("oy"))
+print(myname.startswith("oy"))
+print(myname.find("aaz"))
+print(myname.replace("good","bad"))
+print(myname.capitalize())
+print(myname.count("is"))
