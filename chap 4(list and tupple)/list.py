64e85b3ce46a40348137ca667ae31c51470a17be
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-07-19 11:49:15 -0700

    list

---
+++ chap 4(list and tupple)/list.py
@@ -0,0 +1,15 @@
+names=["ali","ahmad","maaz","hassan"]#list is mutable that we can make any changes to existing list
+print(type(names))
+name=[]#we can also make empty list like that
+print(type(name))
+
+print("ali" in names)#this shows true or false if this value exist in the tuple
+
+print(names[1])#this will print the particular name at this inndex in the list
+names[2]="waqas"#this is allowed in lis because it is mutable
+print(names)
+
+list=[[2,3],4,5,6]#we can also make list in the list this is allowed in the python
+
+
+
---
+++ chap 4(list and tupple)/methods_list.py
@@ -0,0 +1,23 @@
+marks=[20,30,60,90]
+marks.append(34)#this will add the particular value at the end of the list
+#from this we can recognize that list is mutable that we can make changes to existing list
+print(marks)
+marks.remove(20)#this wil remove particular value from the list
+print(marks)
+marks.sort#this will arrange the list
+print(marks)
+marks.reverse#this will reverse the values in the list
+print(marks)
+print(len(marks))
+print (sum(marks))
+print(max(marks))
+print(min(marks))
+marks.insert(2,45)#this will insert the value at your particular index
+print(marks)
+marks.pop(2)#it will remove the value at index 2
+print(marks)
+print(marks.pop(3))#this method will give the value at the index 3
+counts=["C","A","A","D","A"]
+print(counts.count("A"))
+
+    
