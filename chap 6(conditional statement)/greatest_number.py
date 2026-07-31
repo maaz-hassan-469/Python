dabe194ea6e68b115a399a239908ead21931de4b
maaz-hassan-469 <maazhassaan469@gmail.com>
2026-07-31 04:47:54 -0700

    Create greatest_number.py

---
+++ chap 6(conditional statement)/greatest_number.py
@@ -0,0 +1,10 @@
+n1=int(input("enter the first number: "))
+n2=int(input("enter the second number: "))
+n3=int(input("enter the third number: "))
+if(n1>n2&n1>n3):
+    print("n1 is the greatest number")
+elif(n2>n1&n2>n3):
+    print("n2 is the greatest number")
+else:
+    print("n3 is the greatest number")
+
